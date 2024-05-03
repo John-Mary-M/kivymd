fde8089e3dcefc274c4512a83397978fb55613a9
John-Mary-M <johnmaryluxin8@gmail.com>
2024-05-03 05:23:45 +0300

    taking input from the user through the GUI

--- text_input.py
+++ text_input.py
@@ -1,13 +1,14 @@
 '''
 3/05/2024
 Kivymd. Learning to take text input from a user through the gui app i create.
-In this script i also mess around with gridlayout and nested layouts.
+In this script i also mess around with gridlayout.
 
 '''
 
 from kivy.app import App 
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.image import AsyncImage
+# from kivy.uix.image import AsyncImage
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 
@@ -18,7 +19,34 @@ Window.size = (360, 600)
 
 class GetText(App):
     def build(self):
-        pass
+
+        layout = GridLayout(
+           cols=2,
+           row_force_default=True,
+           row_default_height = 40,
+           padding = 20,
+           spacing = 10
+        )
+
+        self.weight=TextInput(text='Enter weight Here: ')
+        self.height=TextInput(text='Enter height Here: ')
+
+        submit = Button(
+            text='Submit',
+            on_press=self.submit
+        )
+
+        layout.add_widget(self.weight)
+        layout.add_widget(self.height)
+        layout.add_widget(submit)
+
+        return layout
+
+    def submit(self, obj):
+        print(f'weight: {self.weight.text}')
+        print(f'height: {self.height.text}')
+        
+        
 
 
 GetText().run()
